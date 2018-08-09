6f0447a51fbaa8d8c1808f2e329cfdcdee900d9f
Elizabeth Chan <lmchan@brynmawr.edu>
2018-08-09 14:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //Code your solution here
+function getFirstSelector(selector) {
+  const first = document.querySelector(selector);
+  return first; 
+}
+
+function nestedTarget() {
+  const nested = document.querySelector('#nested .target');
+  return nested; 
+}
+
+
+function deepestChild() {
+  const deepest = document.querySelector('#grand-node div div div div')
+  return deepest; 
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+ 
+  for (let i = 0; i < lis.length; i++) {
+    let original = lis[i].innerHTML
+    lis[i].innerHTML = (original + 4).toString();
+  }
+}
